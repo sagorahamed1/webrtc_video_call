413f85a9e5d49fbe9c3b8b385903172c5aeb5000
sagorahamed1 <sagorahammed002@gmail.com>
2024-10-04 18:22:39 +0600

    push notification send call done

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,427 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'package:webrtc_video_call/push_notification_server_key.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize Firebase Messaging and Notifications
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'call_channel',
+        channelName: 'Call Notifications',
+        channelDescription: 'Notifications for incoming calls',
+        defaultColor: const Color(0xFF9D50DD),
+        ledColor: Colors.white,
+        importance: NotificationImportance.High,
+        defaultRingtoneType: DefaultRingtoneType.Ringtone,
+        locked: true,
+      )
+    ],
+  );
+
   runApp(const MyApp());
+}
+
+// Firebase messaging background handler
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  showIncomingCallNotification(message.data['callerName']);
+}
+
+// Function to show incoming call notification
+void showIncomingCallNotification(String callerName) {
+  AwesomeNotifications().createNotification(
+    content: NotificationContent(
+      id: 1234,
+      channelKey: 'call_channel',
+      title: 'Incoming Call',
+      body: 'Incoming video call from $callerName',
+      notificationLayout: NotificationLayout.Default,
+      wakeUpScreen: true,
+      fullScreenIntent: true,
+      category: NotificationCategory.Call,
+    ),
+    actionButtons: [
+      NotificationActionButton(key: 'ACCEPT', label: 'Accept', color: Colors.green),
+      NotificationActionButton(key: 'REJECT', label: 'Reject', color: Colors.red),
+    ],
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter WebRTC Call',
+      home: const AuthScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AuthScreenState createState() => _AuthScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AuthScreenState extends State<AuthScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void login() async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    } catch (e) {
+      print('Error logging in: $e');
+    }
+  }
+
+  void register() async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(_auth.currentUser?.uid)
+          .set({'email': emailController.text, 'uid': _auth.currentUser?.uid});
+      login();
+    } catch (e) {
+      print('Error registering: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text('Login/Register')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(controller: emailController, decoration: const InputDecoration(hintText: 'Email')),
+            TextField(controller: passwordController, decoration: const InputDecoration(hintText: 'Password')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(onPressed: login, child: const Text('Login')),
+                ElevatedButton(onPressed: register, child: const Text('Register')),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String? fcmToken;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Configure FCM and store the FCM token in Firestore
+    FirebaseMessaging.instance.getToken().then((token) {
+      fcmToken = token;
+      print('FCM Token: $fcmToken');  // Print FCM token when received
+
+      // Store the FCM token in Firestore for the current user
+      if (_auth.currentUser != null && fcmToken != null) {
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(_auth.currentUser?.uid)
+            .update({'fcmToken': fcmToken});
+      }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      String callerName = message.data['callerName'] ?? 'Unknown Caller';
+      print('Message received: $callerName');
+      showIncomingCallNotification(callerName);
+    });
+
+    // Listen to notification action streams
+    AwesomeNotifications().setListeners(
+      onActionReceivedMethod: (ReceivedAction action) async {
+        if (action.buttonKeyPressed == "REJECT") {
+          print("Call rejected");
+        } else if (action.buttonKeyPressed == "ACCEPT") {
+          print("Call accepted");
+        } else {
+          print("Clicked on notification");
+        }
+      },
+    );
+  }
+
+  void initiateCall(String uid, String peerEmail) async {
+    final callId = FirebaseFirestore.instance.collection('calls').doc().id;
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => CallScreen(callId: callId, isCaller: true, peerId: uid)));
+
+    // Retrieve the recipient's FCM token from Firestore
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    String? peerFcmToken = userDoc['fcmToken'];
+
+    if (peerFcmToken == null) {
+      print('No FCM token found for the user: $peerEmail');
+      return;
+    }
+
+    // Print debug message and send push notification using FCM
+    print('Sending push notification to: $peerEmail (UID: $uid) with FCM token: $peerFcmToken');
+
+    // Get access token from service account
+    String accessToken = await PushNotificationServerKey.getAccessToken();
+    print("Access Token: $accessToken");
+
+    // Send push notification via FCM
+    await sendPushNotification(accessToken, peerFcmToken, callId);
+  }
+
+  Future<void> sendPushNotification(String accessToken, String fcmToken, String callId) async {
+    try {
+      final response = await http.post(
+        Uri.parse("https://fcm.googleapis.com/v1/projects/push-474fb/messages:send"),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $accessToken',
+        },
+        body: jsonEncode(
+          <String, dynamic>{
+            'message': {
+              'notification': {
+                'title': "Incoming Call",
+                'body': "You have an incoming call",
+              },
+              'token': fcmToken,  // Use the recipient's FCM token
+              'data': {
+                'callerId': _auth.currentUser?.uid,
+                'callId': callId,
+                "callerName": _auth.currentUser?.email ?? "Unknown Caller",
+              },
+            },
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        print("Notification sent successfully");
+      } else {
+        print("Failed to send notification: ${response.body}");
+      }
+    } catch (e) {
+      print("Error sending push notification: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Logged In: ${_auth.currentUser?.email}'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await _auth.signOut();
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => const AuthScreen()));
+            },
+          ),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Center(child: CircularProgressIndicator());
+          var users = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              var user = users[index];
+              return ListTile(
+                title: Text(user['email']),
+                trailing: IconButton(
+                  icon: const Icon(Icons.call),
+                  onPressed: () {
+                    initiateCall(user['uid'], user['email']);
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CallScreen extends StatefulWidget {
+  final String callId;
+  final bool isCaller;
+  final String peerId;
+
+  const CallScreen({super.key, required this.callId, required this.isCaller, required this.peerId});
+
+  @override
+  _CallScreenState createState() => _CallScreenState();
+}
+
+class _CallScreenState extends State<CallScreen> {
+  final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
+  final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+  RTCPeerConnection? _peerConnection;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    initRenderers();
+    startCall();
+  }
+
+  @override
+  void dispose() {
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+    _peerConnection?.close();
+    super.dispose();
+  }
+
+  void initRenderers() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+  }
+
+  Future<void> startCall() async {
+    final Map<String, dynamic> configuration = {
+      'iceServers': [
+        {'urls': 'stun:stun.l.google.com:19302'},
+      ],
+    };
+
+    _peerConnection = await createPeerConnection(configuration);
+
+    // Listen for ICE candidates and update Firestore
+    _peerConnection?.onIceCandidate = (RTCIceCandidate candidate) async {
+      if (widget.isCaller) {
+        await _firestore.collection('calls').doc(widget.callId).set({
+          'callerIceCandidates': [],
+          'calleeIceCandidates': [],
+          'offer': null,
+          'answer': null,
+        }, SetOptions(merge: true));  // Create or merge the document
+
+        await _firestore.collection('calls').doc(widget.callId).update({
+          'callerIceCandidates': FieldValue.arrayUnion([candidate.toMap()])
+        });
+      } else {
+        await _firestore.collection('calls').doc(widget.callId).set({
+          'callerIceCandidates': [],
+          'calleeIceCandidates': [],
+          'offer': null,
+          'answer': null,
+        }, SetOptions(merge: true));  // Create or merge the document
+
+        await _firestore.collection('calls').doc(widget.callId).update({
+          'calleeIceCandidates': FieldValue.arrayUnion([candidate.toMap()])
+        });
+      }
+    };
+
+    _peerConnection?.onAddStream = (MediaStream stream) {
+      _remoteRenderer.srcObject = stream;
+    };
+
+    if (widget.isCaller) {
+      final offer = await _peerConnection?.createOffer();
+      await _peerConnection?.setLocalDescription(offer!);
+
+      await _firestore.collection('calls').doc(widget.callId).set({
+        'offer': offer?.toMap(),
+        'callerIceCandidates': [],
+        'calleeIceCandidates': [],
+        'answer': null,
+      }, SetOptions(merge: true));  // Create or merge the document
+    } else {
+      _firestore.collection('calls').doc(widget.callId).snapshots().listen((callDoc) async {
+        if (callDoc.exists && callDoc.data()?['offer'] != null) {
+          await _peerConnection?.setRemoteDescription(
+              RTCSessionDescription(callDoc.data()?['offer']['sdp'], callDoc.data()?['offer']['type']));
+          final answer = await _peerConnection?.createAnswer();
+          await _peerConnection?.setLocalDescription(answer!);
+
+          await _firestore.collection('calls').doc(widget.callId).set({
+            'answer': answer?.toMap(),
+          }, SetOptions(merge: true));  // Create or merge the document
+        }
+
+        if (callDoc.data()?['callerIceCandidates'] != null) {
+          for (var iceCandidate in callDoc.data()?['callerIceCandidates']) {
+            await _peerConnection?.addCandidate(
+              RTCIceCandidate(iceCandidate['candidate'], iceCandidate['sdpMid'], iceCandidate['sdpMLineIndex']),
+            );
+          }
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Call'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.call_end),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          RTCVideoView(_remoteRenderer), // Display remote video
+          Align(
+            alignment: Alignment.topLeft,
+            child: RTCVideoView(_localRenderer), // Display local video
+          ),
+        ],
+      ),
     );
   }
 }
